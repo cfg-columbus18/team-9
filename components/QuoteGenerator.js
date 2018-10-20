1e69c493c2eaa5991ce8adcdb76920d0554c49fc
Adjoa <adjoa.darien@gmail.com>
2018-10-20 01:06:57 -0400

    init QuoteGenerator component

---
+++ components/QuoteGenerator.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Button, Image, StyleSheet, Text, View } from 'react-native';
+
+class QuoteGenerator extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Hello World!</Text>
+      </View>
+    )
+  }
+}
+
+export default QuoteGenerator;
